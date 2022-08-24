0841e7391a41dc16d67c32ef922f406dc774f012
Simon Willison <swillison@gmail.com>
2022-08-24 15:21:36 -0700

    Initial implementation

--- datasette_multiline_links/__init__.py
+++ datasette_multiline_links/__init__.py
@@ -1,1 +1,19 @@
 from datasette import hookimpl
+from datasette.utils import is_url
+from markupsafe import Markup, escape
+
+
+@hookimpl
+def render_cell(value):
+    if not isinstance(value, str):
+        return
+    output = []
+    for line in value.split("\n"):
+        stripped = line.strip()
+        if is_url(stripped):
+            output.append('<a href="{url}">{url}</a>'.format(
+                url=escape(url)
+            )
+        else:
+            output.append(escape(line))
+    return Markup("\n".join(output))
